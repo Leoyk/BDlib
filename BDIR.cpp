5c931c83665bf994588e352c1553478ea2857522
Leoyk <leo.yk@qq.com>
2018-11-23 19:28:37 +0800

    增加IR控制
    增加对红外板的支持，学习和发送。

---
+++ BDIR.cpp
@@ -0,0 +1,19 @@
+#include"BDIR.h"
+#include <Wire.h>
+#include <Arduino.h>
+
+
+#define ZTAddress 0X30  
+
+void learnIR(int a){
+  Wire.beginTransmission(0x30);//对0x30设备进行控制
+  Wire.write("Learn");    //发出学习指令
+  Wire.write(a);          //将学习指令命名为a号指令
+  Wire.endTransmission(); //结束通讯
+}
+void actIR(int a){
+  Wire.beginTransmission(0x30);//对0x30设备进行控制
+  Wire.write("Learn");    //发出学习指令
+  Wire.write(a);          //将学习指令命名为a号指令
+  Wire.endTransmission(); //结束通讯
+}
---
+++ BDIR.h
@@ -0,0 +1,10 @@
+#ifndef _BDIR_H
+#define _BDIR_H
+
+//IIC
+#include "Wire.h"
+
+void learnIR(int a);
+void actIR(int a);
+
+#endif
