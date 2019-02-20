631275fa3b1a2e4bacfc7f5e5647374577224b2d
Leoyk <leo.yk@qq.com>
2019-02-20 20:05:42 +0800

    增加对新版界面的驱动
    改变了原版指令方式，增加了4个新的功能函数（V2界面）

--- BDLCD.cpp
+++ BDLCD.cpp
@@ -1,57 +1,39 @@
 #include"BDLCD.h"
 #include "BD12864.h"//控制0x45单色液晶
 #include <Wire.h>
-#include <Arduino.h>
+
 
 
 void goToPage(int a){
 
+String sBuf = "page page" + (String)a; 
+
  sendStart();
- send2YJ("page page");
- send1Da(a);
+ send2YJ(sBuf);
  sendEnd();
  }
  
  
  
  void drawLine(char a1,unsigned int data){
+
+String sBuf = ""; 
+	 
+	 
 sendStart();
 
 switch(a1){
-case 'B':send2YJ("add 1,0,");break;
-case 'Y':send2YJ("add 1,1,");break;
-case 'R':send2YJ("add 1,2,");break;
-case 'G':send2YJ("add 1,3,");break;
+case 'B':sBuf = "add 1,0," + (String)data;break;
+case 'Y':sBuf = "add 1,1," + (String)data;break;
+case 'R':sBuf = "add 1,2," + (String)data;break;
+case 'G':sBuf = "add 1,3," + (String)data;break;
 default:break;
 } 
-send3Da(data);
+
+send2YJ(sBuf);
 sendEnd();
  }
  
-
-
-void send1Da(unsigned char da1){
-static char numChar1[1];
-
-numChar1[0] = (char)(da1 % 10 + 48);
-send2YJ(numChar1);
-}
-void send3Da(unsigned char da){
-static char numChar[3];
-
-numChar[2] = (char)(da % 10 + 48);
-numChar[1] = (char)(da / 10 % 10 + 48);
-numChar[0] = (char)(da / 100 % 10 + 48);
-send2YJ(numChar);
-}
-
-
-
-
-
-
-
-
 
 
 
@@ -70,16 +52,18 @@ sendEnd();
 delay(10); 
 }
 
-void send2YJ(char *str){
+void send2YJ(String str){
+	
    int sci = 0;
-  if(strlen(str) < 32){
+   
+  if(str.length() < 32){
     Wire.beginTransmission(0x35);//对0x35设备进行控制
-    for(sci = 0;sci < strlen(str);sci ++){     
+    for(sci = 0;sci < str.length();sci ++){     
       Wire.write(str[sci]);    //发出学习指令 
     }
      Wire.endTransmission(); //结束通讯
   }
-   else if(strlen(str) < 64){
+   else if(str.length() < 64){
       Wire.beginTransmission(0x35);//对0x35设备进行控制
       for(sci = 0;sci < 32;sci ++){
        Wire.write(str[sci]);    //发出学习指令 
@@ -91,4 +75,64 @@ void send2YJ(char *str){
       }
        Wire.endTransmission(); //结束通讯
     }
+}
+
+
+char cbuf[32];
+void changeTxt(int id,String txt){
+	
+String sBuf = "";
+
+
+sBuf = "t" + (String)id + ".txt=\"" + txt + "\"";
+
+
+
+ sendStart();
+ send2YJ(sBuf);
+ sendEnd();
+}
+
+
+
+void showTxt(int id,char* txt){
+  String t = txt;
+  changeTxt(id,t);
+  }
+
+void showTxtNum(int id,char* txt,double num){
+  String t = txt + (String)num;
+  changeTxt(id,t);
+  }
+
+  
+  double minV,maxV,stepV;
+
+void setRange(double min,double max){
+
+
+
+double step = (max - min)/10;
+double ii;
+String sii;
+
+minV = min;
+maxV = max;
+stepV = step;
+
+for (int i = 0; i < 11; i ++){
+  ii= min + i * step; 
+  
+ if((ii - (long)ii)  != 0){
+   sii = (String)ii;
+  }
+ else
+   sii = (String)((long)ii);
+   
+char cii[10];
+ for(int j = 0;j < 10;j++){
+   cii[j] = sii[j];
+    }
+  showTxt(i,cii);
+  }
 }
--- BDLCD.h
+++ BDLCD.h
@@ -1,13 +1,16 @@
 #ifndef _BDLCD_H
 #define _BDLCD_H
+#include <Arduino.h>
 
 void goToPage(int a);
 void drawLine(char a1,unsigned int data);
-void send3Da(unsigned char da);
-void send1Da(unsigned char da1);
 void sendEnd();
 void sendStart();
-void send2YJ(char *str);
+void send2YJ(String str);
 
+void changeTxt(int id,String txt);//修订ID 字符串
 
+void showTxt(int id,char* txt);//在ID出显示“字符串 ”
+void showTxtNum(int id,char* txt,double num);//在ID处显示“字符串”+数据
+void setRange(double min,double max);//修订范围
 #endif
