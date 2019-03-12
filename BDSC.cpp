ec999a540078f977ff4f80f92acf65b00872613b
Leoyk <leo.yk@qq.com>
2019-03-12 15:23:38 +0800

    no more  centernumber

--- BDSC.cpp
+++ BDSC.cpp
@@ -2,91 +2,101 @@
 #include <Arduino.h>
 
 #define GSMs Serial1
-void SendMsgChar(char *CentreNum,char * PhoneNum,char *str)
+void SendMsgChar(char * PhoneNum,char *str)
 {
   Serial.begin(115200);
   GSMs.begin(9600);
-  delay(10000);
+  delay(50);
   Serial.print((char)26);
-  delay(1000);
+  delay(50);
   
-  GSMs.println("at+cmgf=1");
-  delay(500);
+  GSMs.println("AT+CMGF=1");
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
   
-  GSMs.print("at+csca=\"");
+/*   GSMs.print("at+csca=\"");
   GSMs.print(CentreNum);
-  GSMs.println("\"");
-  delay(500);
+  GSMs.println("\""); */
+  
+  GSMs.println("AT+CSCS=\"GSM\"");
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
   
-  GSMs.println("at+cmgf=1");
-  delay(500);
+
+  GSMs.println("AT+CMGF=1");
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
+
+
   GSMs.print("AT+CMGS=\"");
   GSMs.print(PhoneNum);
   GSMs.println("\"");
   
-  delay(500);
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
   
   GSMs.print(str);
-  delay(3000);
+  delay(300);
   
   
   
   
   GSMs.print((char)26);
   GSMs.println();
-  delay(500);
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
 }
-void SendMsgNum(char *CentreNum,char * PhoneNum,int str)
+void SendMsgNum(char * PhoneNum,int str)
 {
   Serial.begin(115200);
   GSMs.begin(9600);
-  delay(10000);
+  delay(50);
   Serial.print((char)26);
-  delay(1000);
+  delay(50);
   
-  GSMs.println("at+cmgf=1");
-  delay(500);
+  GSMs.println("AT+CMGF=1");
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
   
-  GSMs.print("at+csca=\"");
+/*   GSMs.print("at+csca=\"");
   GSMs.print(CentreNum);
-  GSMs.println("\"");
-  delay(500);
+  GSMs.println("\""); */
+  
+  GSMs.println("AT+CSCS=\"GSM\"");
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
   
-  GSMs.println("at+cmgf=1");
-  delay(500);
+
+  GSMs.println("AT+CMGF=1");
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
+
+
   GSMs.print("AT+CMGS=\"");
   GSMs.print(PhoneNum);
   GSMs.println("\"");
   
-  delay(500);
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
   
   GSMs.print(str);
-  delay(3000);
+  delay(300);
   
   
   
   
   GSMs.print((char)26);
   GSMs.println();
-  delay(500);
+  delay(50);
   while(GSMs.available())
   Serial.print((char)GSMs.read());
 }
--- BDSC.h
+++ BDSC.h
@@ -1,7 +1,7 @@
 #ifndef _BDSC_H
 #define _BDSC_H
 
-void SendMsgChar(char *CentreNum,char * PhoneNum,char *str);
-void SendMsgNum(char *CentreNum,char * PhoneNum,int str);
+void SendMsgChar(char * PhoneNum,char *str);
+void SendMsgNum(char * PhoneNum,int str);
  
 #endif
