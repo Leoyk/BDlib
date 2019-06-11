ecd7cc8d2253fb1f7f15869ad589fafe3aa84986
LeoYk <leo.yk@qq.com>
2019-06-11 16:14:18 +0800

    修复发送指令
    修复bug

--- BDIR.cpp
+++ BDIR.cpp
@@ -13,7 +13,7 @@ void learnIR(int a){
 }
 void actIR(int a){
   Wire.beginTransmission(0x30);//对0x30设备进行控制
-  Wire.write("Learn");    //发出学习指令
+  Wire.write("Act");    //发出学习指令
   Wire.write(a);          //将学习指令命名为a号指令
   Wire.endTransmission(); //结束通讯
 }
